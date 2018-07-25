60be476ec6c7a16e2535b2dde8cef9cf2b9a6346
Jose Renato Farias da Silva <jose.renato@ubistart.com>
2018-07-25 16:48:26 -0300

    baseurl

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
 
 
-export const API_URL = 'http://177.101.200.35/certificados-api/';  
+export const API_URL = 'http://177.101.200.35/certificados-api/api/';  
 export const APP_KEY = 'base64:7e5rV9ZfeAz55LKbaVKyq5ljsiK7qBqh/6lgu1Rs+Gk=';
 
 
